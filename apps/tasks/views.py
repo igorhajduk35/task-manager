a873365c913c3e5c3f60b9e17d8fa3411f2dbcf1
igorhajduk35 <igorhajduk35@gmail.com>
2026-07-22 23:48:26 +0200

    Added filter_backends to TaskViewSet

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -2,12 +2,36 @@ from .models import Task
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import TaskSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    filterset_fields = [
+        "priority",
+        "status",
+        "assigned_to",
+        "created_by"
+    ]
+    search_fields = [
+        "description",
+        "title",
+        "assigned_to__username",
+        "created_by__username"
+    ]
+    ordering_fields = [
+        "date_created",
+        "due_date",
+        "priority"
+    ]
 
     def perform_create(self, serializer):
         serializer.save(created_by=User.objects.get(id=1))
--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.tasks.apps.TasksConfig',
     'rest_framework',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
